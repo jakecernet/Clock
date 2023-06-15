b96267a5d8370a57da1b9fcf3e9dcf12572791ab
Jaka Černetič <117542696+jakecernet@users.noreply.github.com>
2023-06-15 20:58:36 +0200

    stopwatch kinda working

--- script.js
+++ script.js
@@ -5,13 +5,31 @@ var countdown = document.getElementById("countdown")
 function clockDisplay() {
     if (clock.style.display == "none") {
         clock.style.display = "block";
-        console.log("clockDisplay");
     } else {
         clock.style.display = "none";
-        console.log("clockDisplay-none");
     }
     stopwatch.style.display = "none";
     countdown.style.display = "none";
+}
+
+function stopwatchDisplay() {
+    if (stopwatch.style.display == "none") {
+        stopwatch.style.display = "block";
+    } else {
+        stopwatch.style.display = "none";
+    }
+    clock.style.display = "none";
+    countdown.style.display = "none";
+}
+
+function countdownDisplay() {
+    if (countdown.style.display == "none") {
+        countdown.style.display = "block";
+    } else {
+        countdown.style.display = "none";
+    }
+    clock.style.display = "none";
+    stopwatch.style.display = "none";
 }
 
 function clockDisplayInfinite() {
@@ -20,36 +38,48 @@ function clockDisplayInfinite() {
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
     var time = hours + " : " + minutes + " : " + seconds;
-    document.getElementById("clock").innerHTML = time;
-    setTimeout(clockDisplay, 1000);
+    document.getElementById("clock-display").innerHTML = time;
+    setTimeout(clockDisplayInfinite, 1000);
 }
 
-function stopwatchDisplayInfinite() {
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var time = hours + " : " + minutes + " : " + seconds;
-    document.getElementById("stopwatch").innerHTML = time;
-    setTimeout(stopwatchDisplay, 1000);
-    clock.style.display = "none";
-    stopwatch.style.display = "block";
-    countdown.style.display = "none";
+function startStopwatch() { 
+    var time = 0;
+    var interval = setInterval(function() {
+        time++;
+        var hours = Math.floor(time / 10 / 60 / 60);
+        var minutes = Math.floor(time / 10 / 60);
+        var seconds = Math.floor(time / 10 % 60);
+        var tenths = time % 10;
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        if (minutes < 10) {
+            minutes = "" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        document.getElementById("stopwatch-display").innerHTML = hours + " : " + minutes + " : " + seconds + " : " + tenths;
+    }
+    , 100);
+    document.getElementById("start-btn").onclick = function() {
+        clearInterval(interval);
+    }
+    document.getElementById("stop-btn").onclick = function() {
+        clearInterval(interval);
+    }
+    document.getElementById("reset-btn").onclick = function() {
+        clearInterval(interval);
+        resetStopwatch();
+    }
 }
 
-function countdownDisplayInfinite() {
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var time = hours + " : " + minutes + " : " + seconds;
-    document.getElementById("countdown").innerHTML = time;
-    setTimeout(countdownDisplay, 1000);
-    clock.style.display = "none";
-    stopwatch.style.display = "none";
-    countdown.style.display = "block";
+function resetStopwatch() {
+    document.getElementById("stopwatch-display").innerHTML = "00 : 00 : 00 : 0";
 }
 
 clock.style.display = "none";
 stopwatch.style.display = "none";
 countdown.style.display = "none";
+
+clockDisplayInfinite();
